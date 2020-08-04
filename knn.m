2a7ab8ef7badd5e33ad3e4fd7f00982ac2b80979
luci <luci.fernandes@alu.ufc.br>
2020-08-03 21:02:13 -0300

    Segundo commit

--- knn.m
+++ knn.m
@@ -1,7 +1,11 @@
-
-Classe1 = [Classe1 1]
-Classe2 = [Classe2 2]
+c1 = ones(500,1)
+c2 = c1*2
+Classe1 = [Classe1 c1]
+Classe2 = [Classe2 c2]
 base0 = [Classe1;Classe2]
 base=[]
-r = randi([0 99],1,1000)
+r = randsample(1000,1000)
 
+for i=1:1000
+    base = [base base0(r(i,10),:)]
+end
